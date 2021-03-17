54d4848af2df56dd9fb834eff43db20c5ccaaae5
Gaston Singh <gastonsingh@hotmail.com>
2021-03-17 15:10:16 -0300

    express.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+/* Los estaticos de JS estan en public */
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
